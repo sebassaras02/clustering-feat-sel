6ac1194e7d1f20c6587d1913f4b56ddd28d81873
Sebastián Sarasti <sebitas.alejo@hotmail.com>
2024-10-12 14:52:09 -0500

    adding the multithread capability

--- src/featclus.py
+++ src/featclus.py
@@ -1,5 +1,3 @@
-from joblib import Parallel, delayed
-
 import numpy as np
 import pandas as pd
 
@@ -11,6 +9,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import silhouette_score
+
+from joblib import Parallel, delayed
 
 
 class FeatureSelection:
@@ -33,7 +33,7 @@ class FeatureSelection:
         pd.DataFrame: A DataFrame with the importance of each feature sorted.
     """
 
-    def __init__(self, data: pd.DataFrame, shifts: List = [5, 10, 50], n_jobs:int = -1):
+    def __init__(self, data: pd.DataFrame, shifts: List = [5, 10, 50], n_jobs: int = 1):
         self.data = data
         self.shifts = shifts
         self.model = Pipeline(
@@ -46,7 +46,7 @@ class FeatureSelection:
         self.columns = data.columns
         self.n_jobs = n_jobs
 
-    def _shit_data(self, df: pd.DataFrame, target_column: str) -> List[pd.DataFrame]:
+    def _shift_data_sc(self, df: pd.DataFrame, target_column: str) -> List[pd.DataFrame]:
         """
         This function creates different dataframes based on different shifts.
         """
@@ -57,41 +57,51 @@ class FeatureSelection:
             df1 = df1.dropna()
             data_shifted.append(df1)
         return data_shifted
+        
+    def _shift_data_mc(self, df: pd.DataFrame) -> pd.DataFrame:
+        data_shifted = []
+        for col in self.columns:
+            for value in self.shifts:
+                df1 = deepcopy(self.data)
+                df1[col] = df1[col].shift(value)
+                df1 = df1.dropna()
+                data_shifted.append((col, df1))  # Almacena la columna junto con el DataFrame
+        return data_shifted
+
 
     def _train_model(self) -> dict:
         """
-        This function trains the models needed in a parallelized way.
+        This function trains a model for each column with a different data shift.
         """
         if self.n_jobs == 1:
-            scores = {col: self._evaluate_feature(col) for col in self.columns}
+            scores = {}
+            for col in self.columns:
+                df_to_test = self._shift_data_sc(df=self.data, target_column=col)
+                values = []
+                for df in df_to_test:
+                    values.append(self._get_score(df=df))
+                scores[col] = np.mean(values)
+            return scores
         else:
-            scores = Parallel(n_jobs=self.n_jobs)(
-                delayed(self._evaluate_feature)(col) for col in self.columns
-            )
-            scores = dict(zip(self.columns, scores))
-        return scores
+            dataframes = self._shift_data_mc(df=self.data)
+            results = Parallel(n_jobs=self.n_jobs)(delayed(self._get_score)(df) for _, df in dataframes)
+            scores = {col: [] for col in self.columns}
+            for (col, _), score in zip(dataframes, results):
+                scores[col].append(score)
+            return {col: np.mean(scores[col]) for col in scores}
     
-    def _evaluate_feature(self, col: str) -> float:
+    def _process_columns(self, col):
+        df_to_test = self._shift_data(df=self.data, target_column=col)
+        values = []
+        for df in df_to_test:
+            values.append(self._get_score(df=df))
+        return col, np.mean(values)
+
+    def _get_score(self, df) -> float:
         """
-        This function applies the shifts in a parallelized way.
-        """
-        df_to_test = self._shit_data(df=self.data, target_column=col)
-        
-        if self.n_jobs == 1:
-            values = [self._get_score(df) for df in df_to_test]
-        else:
-            values = Parallel(n_jobs=self.n_jobs)(
-                delayed(self._get_score)(df) for df in df_to_test
-            )
-        return np.mean(values)
-    
-    def _get_score(self, df: pd.DataFrame) -> float:
-        """
-        This function calculates the score of the clustering for each variable
+        This function calculates the silhouete score for each model created.
         """
         labels = self.model.fit_predict(df)
-        print(len(labels))
-        print(len())
         score = silhouette_score(X=df, labels=labels)
         return score
 
@@ -108,3 +118,9 @@ class FeatureSelection:
             final_values.values(), columns=["Importance"], index=final_values.keys()
         ).sort_values("Importance", ascending=False)
         return df
+    
+    def plot_results(n_features: None):
+        """
+        This function plots the results of the model.
+        """
+        pass
